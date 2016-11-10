84fdcf9095a5169878b0ba17fe45720324fe4487
David Goodwin <david@palepurple.co.uk>
2016-11-10 10:12:28 +0000

    initial version

---
+++ README.md
@@ -0,0 +1,14 @@
+# What ?
+
+Crude took to check remote ssl certificates.
+
+# Installation
+
+Run via cron (php check.php). If it outputs anything, you probably need to do something.
+
+Requires '''openssl''' and PHP (PHP, because I'm too lazy to do it all in bash)
+
+# Configuration 
+Add hosts to 'hosts.conf' one per line.
+
+By default, it assumes you wish to check port 443 (https). If you want to check a different port then do host:port (e.g. mail.mydomain.com:25)
---
+++ check.php
@@ -0,0 +1,44 @@
+<?php
+
+foreach(file('hosts.conf') as $line) {
+    list($host, $port) = _parse($line);
+    $ok = _check($host, $port);
+    if($ok !== true) {
+        echo "Warning: $host ($port) expires on $ok\n";
+    }
+}
+
+/**
+ * parse a single line, default port to 443 (https) unless specified
+ *
+ * @param string line like 'hostname' or 'hostname:port'
+ * @return array ($host, $port)
+ */
+function _parse($line) {
+    $line = trim($line);
+    if(preg_match('/(.*):(\d+)/', $line, $matches)) {
+        return array($matches[1], $matches[2]);
+    }
+    return array($line, '443');
+}
+
+/**
+ * Check given host/port
+ * @return boolean|string true if it expires more than 2 weeks away, otheriwse expiry date.
+ */
+function _check($host, $port) {
+    $tls_ports = [25 => 'smtp', '143' => 'imap' ];
+    $starttls = '';
+    if(isset($tls_ports[$port])) {
+        $starttls = " -starttls {$tls_ports[$port]} ";
+    }
+    $output = shell_exec("echo | openssl s_client -connect $host:$port -servername $host $starttls 2>/dev/null | openssl x509 -noout -dates");
+    #exec("python ssl_expiry.py -H $host -p $port -n $host", $output, $retval);
+    if(preg_match('/notAfter=(.*)$/',$output, $matches)) {
+        $date = new DateTime($matches[1]);
+        if($date > new DateTime('+2 weeks')) {
+            return true;
+        }
+        return $date->format('Y-m-d');
+    }
+}
