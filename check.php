12e062bdf065ac2605c8d178365234b0065c00ec
David Goodwin <david@codepoets.co.uk>
2024-01-22 20:45:46 +0000

    fix array lookup of port / tls etc (make psalm happy)

--- check.php
+++ check.php
@@ -32,7 +32,9 @@ function _parse(string $line) : array {
  */
 function _check(string $host, string $port) {
 
-    $tls_ports = ['25' => 'smtp', '143' => 'imap', '21' => 'ftp'];
+    $port = (int) $port;
+
+    $tls_ports = [25 => 'smtp', 143 => 'imap', 21 => 'ftp'];
     $starttls = '';
 
     if (array_key_exists($port, $tls_ports)) {
